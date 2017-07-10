46fd50a4c5f5895258948d5d8e63c71ce506e2f6
Stephen Walker <swalker@redhat.com>
2017-07-10 11:08:34 -0400

    Move cli handlers to their own file and make state a struct

---
+++ lib/io_tty/cli_handlers.ex
@@ -0,0 +1,91 @@
+defmodule IOTty.CLIHandlers do
+  @moduledoc """
+    Default config.
+    Used when :default is supplied in start_link, or when there are no arguments given to IOTty.start_link
+    Implements forward, back, return, backspace, delete, home and end keys as you would expect a command line app would.
+  """
+
+  alias IOTty.CLIHandlers, as: State
+  defstruct input: "", cursor: 0, history: [], helem: 0
+
+  @up "\e[A"
+  @dn "\e[B"
+  @fw "\e[C"
+  @bk "\e[D"
+  @ret "\r"
+  @bksp "\d"
+  @del "\e[3~"
+  @home "\e[H"
+  @enk  "\e[F"
+
+  def map() do
+    %{
+      :initial_state => %State{},
+      :default => &handle_press/2,
+     }
+  end
+
+  defp handle_press(@fw, state = %State{input: input, cursor: cursor}) do
+    if String.length(input) >= cursor + 1 do
+      IO.write(@fw)
+      %{state | input: input, cursor: cursor+1}
+    else 
+      %{state | input: input, cursor: cursor}
+    end
+  end
+
+  defp handle_press(@bk, state = %State{cursor: cursor}) do
+    case cursor do
+      0 -> 
+        state
+      _ ->
+        IO.write(@bk)
+        %{state | cursor: cursor-1}
+    end
+  end
+
+  defp handle_press(@ret, state = %State{input: input}) do
+    IO.write("\n\e[E")
+    {:send_line, input, %{state | input: "", cursor: 0}}
+  end
+
+  defp handle_press(@bksp, state = %State{input: input, cursor: cursor}) do
+    {pre, post} = cut(input, cursor)
+    case cursor do
+      0 -> 
+        state
+      _ ->
+        IO.write(@bk <> "\e[K" <> post <> back_up(post))
+        %{state | input: String.slice(pre, 0..-2) <> post, cursor: cursor-1}
+    end
+  end
+
+  defp handle_press(@del, state = %State{cursor: cursor}) do
+    IO.write(@fw)
+    handle_press(@bksp, %{state | cursor: cursor+1})
+  end
+
+  defp handle_press(@home, state = %State{}) do
+    IO.write("\e[u")
+    %{state | cursor: 0}
+  end
+
+  defp handle_press(@enk, state = %State{input: input}) do
+    IO.write("\e[u" <> String.duplicate(@fw, String.length(input)))
+    %{state | cursor: String.length(input) - 1}
+  end
+
+  defp handle_press(<< b >>, state = %State{input: input, cursor: cursor})
+    when b in 32..126 do
+    {pre, post} = cut(input, cursor)
+    IO.write(<< b >> <> "\e[K" <> post <> back_up(post))
+    %{state | input: pre <> << b >> <> post, cursor: cursor+1}
+  end
+
+  defp handle_press(_, state), do: state
+
+  defp cut(input, cursor) when cursor == 0, do: {"", input}
+  defp cut(input, cursor), do: {String.slice(input, 0..cursor-1), String.slice(input, cursor..-1)}
+  defp back_up(string), do: String.duplicate(@bk, String.length(string))
+
+end
--- lib/io_tty/key_handlers.ex
+++ lib/io_tty/key_handlers.ex
@@ -7,7 +7,7 @@ defmodule IOTty.KeyHandlers do
   @doc """
     Start the key handlers, should be called from the main IOTty.start_link
   """
-  def start_link(:default), do: GenServer.start_link(__MODULE__, default_config(), name: :key_handlers) 
+  def start_link(:default), do: GenServer.start_link(__MODULE__, IOTty.CLIHandlers.map(), name: :key_handlers) 
   def start_link(:debug), do: GenServer.start_link(__MODULE__, debug_config(), name: :key_handlers) 
   def start_link(config), do: GenServer.start_link(__MODULE__, config, name: :key_handlers)
 
@@ -45,31 +45,12 @@ defmodule IOTty.KeyHandlers do
   """
   def handle_call(:initial_state, _from, func_map), do: {:reply, func_map[:initial_state], func_map}
 
-  @fw "\e[C"
-  @bk "\e[D"
-  @ret "\r"
-  @bksp "\d"
-  @del "\e[3~"
-  @home "\e[H"
-  @enk  "\e[F"
-
-  @doc """
-    Default config.
-    Used when :default is supplied in start_link, or when there are no arguments given to IOTty.start_link
-    Implements forward, back, return, backspace, delete, home and end keys as you would expect a command line app would.
-  """
-  def default_config() do
-    %{
-      :initial_state => {"", 0},
-      :default => &handle_press/2,
-     }
-  end
 
   @doc """
     Debug config.
     Used when :debug is supplied in start_link, or when given to IOTty.start_link
     Only outputs the string equivalents to key presses.
-    Helpful when creating callback map
+    Helpful when creating callback map.
   """
   def debug_config() do
     %{
@@ -79,66 +60,8 @@ defmodule IOTty.KeyHandlers do
    end
 
   defp handle_debug(key, state) do
-    IO.puts("KEY: #{inspect key}\e[E")
+    IOTty.puts("KEY: #{inspect key}\e[E")
     state
   end
-
-  defp handle_press(@fw, {input, cursor}) do
-    if String.length(input) >= cursor + 1 do
-      IO.write(@fw)
-      {input, cursor+1}
-    else 
-      {input, cursor}
-    end
-  end
-  defp handle_press(@bk, {input, cursor}) do
-    case cursor do
-      0 -> 
-        {input, cursor}
-      _ ->
-        IO.write(@bk)
-        {input, cursor-1}
-    end
-  end
-  defp handle_press(@ret, state = {input, _}) do
-    IO.write("\n\e[E")
-    {:send_line, input, {"", 0}}
-  end
-
-  defp handle_press(@bksp, {input, cursor}) do
-    {pre, post} = cut(input, cursor)
-    case cursor do
-      0 -> 
-        {post , 0}
-      _ ->
-        IO.write(@bk <> "\e[K" <> post <> back_up(post))
-        {String.slice(pre, 0..-2) <> post , cursor-1}
-    end
-  end
-
-  defp handle_press(@del, {input, cursor}) do
-    IO.write(@fw)
-    handle_press(@bksp, {input, cursor+1})
-  end
-
-  defp handle_press(@home, {input, _}) do
-    IO.write("\e[u")
-    {input, 0}
-  end
-
-  defp handle_press(@enk, {input, _}) do
-    IO.write("\e[u" <> String.duplicate(@fw, String.length(input)))
-    {input, String.length(input) - 1}
-  end
-
-  defp handle_press(<< b >>, {input, cursor}) when b in 32..126 do
-    {pre, post} = cut(input, cursor)
-    IO.write(<< b >> <> post <> back_up(post))
-    {pre <> << b >> <> post, cursor+1}
-  end
-  defp handle_press(_, state), do: state
-
-  defp cut(input, cursor), do: {String.slice(input, 0..cursor-1), String.slice(input, cursor..-1)}
-  defp back_up(string), do: String.duplicate(@bk, String.length(string))
 
 end
