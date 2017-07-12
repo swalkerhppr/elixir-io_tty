53b5a4f1642bb37b278e25beb175e323d0873896
Stephen Walker <swalker@redhat.com>
2017-07-12 10:25:15 -0400

    Fix need to have compile time config value

--- lib/io_tty/helpers.ex
+++ lib/io_tty/helpers.ex
@@ -1,12 +1,18 @@
 defmodule IOTty.Helpers do
 
   @debug_file Application.get_env(:io_tty, :debug_out_file)
-  @default_out Application.get_env(:io_tty, :default_out)
+
+  defp default_out do
+    case env_set = Application.get_env(:io_tty, :default_out) do
+      nil -> :stdio
+      _   -> env_set 
+    end
+  end
 
   @doc """
     Helper to Write to IO.
   """
-  def write(device \\ @default_out, msg)
+  def write(device \\ default_out(), msg)
   def write(:debug, msg), do: File.write(@debug_file, msg)
   def write(:stdio, msg), do: IO.write(:stdio, msg)
 end
